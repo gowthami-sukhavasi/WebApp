a8997b3d5bbcbf65c50eac198b396f5cd8bb66ad
Gowthami Sukhavasi <gowthamisukhavasi@yahoo.in>
2021-05-14 19:58:19 -0400

    Add project files.

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query = @"
+            select * 
+            from
+            dbo.Employee
+            ";
+            DataTable table = new DataTable();
+            using(var con= new SqlConnection(ConfigurationManager.
+                ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+          using (var da= new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+
+        }
+
+        public string Post(Employee emp)
+        {
+            try
+            {
+                string query = @"
+                insert into dbo.Employee values
+                 (
+                 '" + emp.EmployeeName + @"',
+                 '" + emp.EmployeeAddress + @"',
+                 '" + emp.EmployeePhone + @"',
+                 '" + emp.EmployeePosition + @"'
+                  )";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Added successfully!";
+            }
+            catch(Exception)
+            {
+                return "Failed to add!";
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"
+                 delete from dbo.Employee
+                 where EmployeeId=" + id + @"
+                 ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Deleted successfully!";
+            }
+            catch (Exception)
+            {
+                return "Failed to delete!";
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeAddress { get; set; }
+        public string EmployeePhone { get; set; }
+        public string EmployeePosition { get; set; }
+    }
+}
